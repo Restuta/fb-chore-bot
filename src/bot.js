d49dc5bb2eee52fd5a3bddea542bf3ea4f1718e9
Restuta <restuta8@gmail.com>
2016-12-03 12:51:02 -0800

    added cloudia config and test bot is working

--- src/bot.js
+++ src/bot.js
@@ -3,5 +3,5 @@ import botBuilder from 'claudia-bot-builder'
 export default botBuilder(function(message) {
   console.info('got msg: ' + message.text)
 
-  return 'Got you! You wrote ' + message.text
+  return 'Got you! You wrote ' + message.text + ' and extra 🦄'
 })
